3fe93a73d87fa45ba34956109096e9dc52dcdb83
Oliver Lonsdale <oliver.lonsdale@fourquant.com>
2023-12-28 01:03:13 +0000

    Add project files.

---
+++ CrossStitchApp/Models/SaveFile.cs
@@ -0,0 +1,14 @@
+﻿namespace CrossStitchApp.Models
+{
+    public class SaveFile
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public Tile[] Data { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public HashSet<string> Colors { get; set; } = new();
+
+    }
+}
---
+++ CrossStitchApp/Models/Tile.cs
@@ -0,0 +1,16 @@
+﻿namespace CrossStitchApp.Models
+{
+    public class Tile
+    {
+        public string? Color { get; set; }
+        public bool Stitched { get; set; } = false;
+
+        public Tile(string Color, bool Stitched) 
+        {
+            this.Color = Color;
+            this.Stitched = Stitched;
+        }
+
+        public Tile() { }
+    }
+}
